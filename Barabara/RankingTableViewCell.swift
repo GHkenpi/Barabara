5bc523ecd9191d324a2fec0f3c2ad79c2ac60dd4
KensukeNakane <Ken.20153195@gmail.com>
2023-05-12 03:56:46 +0900

    Initial Commit

---
+++ Barabara/RankingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RankingTableViewCell.swift
+//  Barabara
+//
+//  Created by 中根健介 on 2023/05/12.
+//
+
+import UIKit
+
+class RankingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
